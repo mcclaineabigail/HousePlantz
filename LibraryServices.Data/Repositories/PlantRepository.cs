988bd81a682eec69349dd92437d4e7d8303e2a0c
Abigail Mcclaine <Abigail.Mcclaine@insight.com>
2021-02-15 15:51:21 -0500

    test 1

--- LibraryServices.Data/Repositories/PlantRepository.cs
+++ LibraryServices.Data/Repositories/PlantRepository.cs
@@ -12,7 +12,7 @@ namespace HousePlantz.Data.Repositories
 
         public List<Plant> plants = new List<Plant>()
         {
-            new Plant {Id = 1, Name = "Peperomia", Sun = "/light/low-med.png",  Image = "/plant-images/peperomia.PNG", Water = "Allow to Dry", Notes = "Poisonous"},
+            new Plant {Id = 1, Name = "Peperomia test 1", Sun = "/light/low-med.png",  Image = "/plant-images/peperomia.PNG", Water = "Allow to Dry", Notes = "Poisonous"},
             new Plant {Id = 2, Name = "Chinese Evergreen", Sun = "/light/low-med.png",  Image = "/plant-images/chinese-evergreen.PNG", Water = "Keep Evenly Moist", Notes = "Poisonous"},
             new Plant {Id = 3, Name = "Grape Ivy", Sun = "/light/med.png",  Image = "/plant-images/grape-ivy.PNG", Water = "Keep Evenly Moist", Notes = "Trailing"},
             new Plant {Id = 4, Name = "Norfolk Island Pine", Sun = "/light/bright.png",  Image = "/plant-images/norfolk-island-pine.PNG", Water = "Allow to Dry", Notes = "Festive"},
