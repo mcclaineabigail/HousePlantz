fd01e1f09705e4405a95b3beeef3191613296bdc
Abigail Mcclaine <Abigail.Mcclaine@insight.com>
2021-02-25 07:54:33 -0500

    working delete request

--- HousePlantz/Controllers/TextController.cs
+++ HousePlantz/Controllers/TextController.cs
@@ -1,5 +1,6 @@
 ﻿using HousePlantz.Data.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -23,7 +24,7 @@ namespace HousePlantz.Controllers
         [HttpPost]
         public Plant PostLineToTextFile([FromBody] Plant plantToAdd)
         {
-            var jsonText = JsonSerializer.Serialize(plantToAdd, new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            var jsonText = System.Text.Json.JsonSerializer.Serialize(plantToAdd, new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
             string allPath = @"C:\Users\amcclain\source\repos\HousePlantz\LibraryServices.Data\Text\CatalogText.txt";
             string startingText = System.IO.File.ReadAllText(allPath);
             string newText = startingText + "," + jsonText; //Get rid of Shotty Code ya newb
@@ -43,20 +44,12 @@ namespace HousePlantz.Controllers
         public void Delete(long id)
         {
             var allText = GetText();
-            var plantList = JsonSerializer.Deserialize<List<Plant>>(allText);
 
-        //    List<Plant> plantList = new List<Plant>()
-        //{
-        //    new Plant {Id = 1, Name = "Peperomia", Sun = "low-medium", Image = "peperomia", Water = "Allow to Dry", Notes = "Poisonous"},
-        //    new Plant {Id = 2, Name = "Chinese Evergreen", Sun = "low-medium", Image = "chinese-evergreen", Water = "Keep Evenly Moist", Notes = "Poisonous"},
-        //    new Plant {Id = 9, Name = "ZZ Plant", Sun = "any", Image = "zz-plant", Water = "Allow to Dry", Notes = "Poisonous"},
-        //    new Plant {Id = 0, Name = "string", Sun = "string", Image = "string", Water = "string", Notes = "string"}
-
-        //};
+            var plantList = JsonConvert.DeserializeObject<List<Plant>>(allText);
 
             var plantToRemove = plantList.FirstOrDefault(x => x.Id == id);
             plantList.Remove(plantToRemove);
-            var newText = JsonSerializer.Serialize(plantList);
+            var newText = System.Text.Json.JsonSerializer.Serialize(plantList);
 
             string allPath = @"C:\Users\amcclain\source\repos\HousePlantz\LibraryServices.Data\Text\CatalogText.txt";
 
@@ -65,3 +58,41 @@ namespace HousePlantz.Controllers
         }
     }
 }
+
+
+
+// This version is able to handle one Plant, possibly try to split up the string by , if the deserialize object doesn't work.
+
+//[HttpDelete("{id}")]
+//public void Delete(long id)
+//{
+//    var allText = GetText();
+
+//    var plant = JsonSerializer.Deserialize<Plant>(allText);
+//    var plantList = new List<Plant> { plant };
+
+//    var plantToRemove = plantList.FirstOrDefault(x => x.Id == id);
+//    plantList.Remove(plantToRemove);
+//    var newText = JsonSerializer.Serialize(plantList);
+
+//    string allPath = @"C:\Users\amcclain\source\repos\HousePlantz\LibraryServices.Data\Text\CatalogText.txt";
+
+//    using var sw = new StreamWriter(allPath);
+//    sw.WriteLine(newText);
+
+
+
+
+// Original code
+
+//var allText = GetText();
+//var plantList = JsonSerializer.Deserialize<List<Plant>>(allText);
+
+//var plantToRemove = plantList.FirstOrDefault(x => x.Id == id);
+//plantList.Remove(plantToRemove);
+//var newText = JsonSerializer.Serialize(plantList);
+
+//string allPath = @"C:\Users\amcclain\source\repos\HousePlantz\LibraryServices.Data\Text\CatalogText.txt";
+
+//using var sw = new StreamWriter(allPath);
+//sw.WriteLine(newText);
