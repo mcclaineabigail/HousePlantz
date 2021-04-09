92ec403d3bca46397f9e97cefab866099b0f52af
Abigail Mcclaine <Abigail.Mcclaine@insight.com>
2021-04-09 15:21:36 -0400

    Angular GET ownedPlants

--- angular/src/app/catalog/catalog.component.ts
+++ angular/src/app/catalog/catalog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { CatalogService } from './catalog.service';
 import { IOwnedPlant } from './models/ownedPlant';
+import { IPlant } from './models/plant';
 
 @Component({
   selector: 'hp-catalog',
@@ -9,59 +10,8 @@ import { IOwnedPlant } from './models/ownedPlant';
   styleUrls: ['./catalog.component.scss']
 })
 export class CatalogComponent implements OnInit {
-  plantCatalog: IOwnedPlant[] = [
-    {
-      "id": 1,
-      "nickName": "Plant 1",
-      "plant": {
-          "id": 1,
-          "name": "Pothos",
-          "sun": "https://github.com/mcclaineabigail/PlantCatalog/blob/main/HousePlantz.Data/Images/light/low-medium.png?raw=true",
-          "image": "https://github.com/mcclaineabigail/PlantCatalog/blob/main/HousePlantz.Data/Images/light/low-medium.png?raw=true",
-          "water": "Keep Evenly Moist",
-          "notes": "Plant 1 Notes"
-          },
-      "room": {
-          "id":  1,
-          "roomName": "Kitchen", 
-          "color": "Red"
-              }
-      },
-      {
-      "id": 2,
-      "nickName": "Plant 2",
-      "plant": {
-          "id": 1,
-          "name": "Pothos",
-          "sun": "https://github.com/mcclaineabigail/PlantCatalog/blob/main/HousePlantz.Data/Images/light/low-medium.png?raw=true",
-          "image": "https://github.com/mcclaineabigail/PlantCatalog/blob/main/HousePlantz.Data/Images/light/low-medium.png?raw=true",
-          "water": "Keep Evenly Moist",
-          "notes": "Plant 2 Notes"
-          },
-      "room": {
-          "id":  1,
-          "roomName": "Kitchen", 
-          "color": "Red"
-              }
-      },
-      {
-      "id": 3,
-      "nickName": "Plant 3",
-      "plant": {
-          "id": 1,
-          "name": "Pothos",
-          "sun": "https://github.com/mcclaineabigail/PlantCatalog/blob/main/HousePlantz.Data/Images/light/low-medium.png?raw=true",
-          "image": "https://github.com/mcclaineabigail/PlantCatalog/blob/main/HousePlantz.Data/Images/light/low-medium.png?raw=true",
-          "water": "Keep Evenly Moist",
-          "notes": "Plant 3 Notes"
-          },
-      "room": {
-          "id":  1,
-          "roomName": "Kitchen", 
-          "color": "Red"
-              }
-      }
-  ]
+  catalog: IOwnedPlant[] = [];
+  plants: IPlant[] = [];
   sub!: Subscription;
   errorMessage: string = '';
   toggleClass: boolean = false;
@@ -69,10 +19,14 @@ export class CatalogComponent implements OnInit {
   constructor(private catalogService: CatalogService) { }
 
   ngOnInit(): void {
-    // this.sub = this.catalogService.getPlants().subscribe({
-    //   next: plantCatalog => this.plantCatalog = plantCatalog,
-    //   error: err => this.errorMessage = err
-    //})
+    this.sub = this.catalogService.getPlantList().subscribe({
+      next: plants => this.plants = plants,
+      error: err => this.errorMessage = err
+    });
+    this.sub = this.catalogService.getCatalog().subscribe({
+      next: catalog => this.catalog = catalog,
+      error: err => this.errorMessage = err
+    });
   }
 
   flipCard(): void{
--- angular/src/app/catalog/catalog.service.ts
+++ angular/src/app/catalog/catalog.service.ts
@@ -3,26 +3,36 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { IOwnedPlant } from './models/ownedPlant';
+import { IPlant } from './models/plant';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CatalogService {
-  //  private catalogUrl = 'api/catalog/sampleCatalog.json';
-  //   constructor(private http: HttpClient) {}
-  // getPlants(): Observable<IOwnedPlant[]> {
-  //     return this.http.get<IOwnedPlant[]>(this.catalogUrl);
-  //     tap(data => console.log('All', JSON.stringify(data))),
-  //     catchError(this.handleError)
-  // }
-  // private handleError(err: HttpErrorResponse): Observable<never> {
-  //     let errorMessage = '';
-  //     if (err.error instanceof ErrorEvent) {
-  //       errorMessage = `An error occurred: ${err.error.message}`;
-  //     } else {
-  //       errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
-  //     }
-  //     console.error(errorMessage);
-  //     return throwError(errorMessage);
-  //   }
+   private catalogUrl = 'https://localhost:44313/api/ownedplants';
+   private plantUrl = 'https://localhost:44313/api/plants'
+    constructor(private http: HttpClient) {}
+
+
+  getCatalog(): Observable<IOwnedPlant[]> {
+      return this.http.get<IOwnedPlant[]>(this.catalogUrl);
+      tap(data => console.log('All', JSON.stringify(data))),
+      catchError(this.handleError)
+  }
+
+  getPlantList(): Observable<IPlant[]>{
+    return this.http.get<IPlant[]>(this.plantUrl);
+    tap(data => console.log('All', JSON.stringify(data))),
+    catchError(this.handleError)
+  }
+  private handleError(err: HttpErrorResponse): Observable<never> {
+      let errorMessage = '';
+      if (err.error instanceof ErrorEvent) {
+        errorMessage = `An error occurred: ${err.error.message}`;
+      } else {
+        errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+      }
+      console.error(errorMessage);
+      return throwError(errorMessage);
+    }
 }
